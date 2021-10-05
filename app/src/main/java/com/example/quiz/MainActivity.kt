6897f559ad2699f043307847d1a8ead088f2cef9
per2 <diegoalexwilliams33@gmail.com>
2021-10-05 10:06:06 -0700

    Basic frame almost complete

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import org.json.JSONArray
+import org.json.JSONObject
 
 lateinit var questionText : TextView
 lateinit var trueButton: Button
 lateinit var falseButton: Button
+val TAG = "MainActivity"
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +23,39 @@ class MainActivity : AppCompatActivity() {
 
         wireWidgits()
 
+        lateinit var quiz : Quiz
+
+        val inputStream = resources.openRawResource(R.raw.questions)
+        val jsonText = inputStream.bufferedReader().use {
+            it.readText()
+        }
+        Log.d(TAG, "onCreate: $jsonText")
+
+        val gson = Gson()
+        val qType = object : TypeToken<List<Question>>() { }.type
+        val questionList = gson.fromJson<List<Question>>(jsonText, qType)
+        Log.d(TAG, "onCreate: ${questionList.toString()}")
+
+        questions = List<Question>
+
+        val jsonArray = JSONArray(jsonText)
+        for(question:JSONObject in jsonArray){
+            val questionText = question.get("question")
+            val questionAnswer = question.get("answer")
+            val question = Question(questionText, questionAnswer)
+            questions.add(question)
+        }
+
+        quiz = Quiz(questions)
+    }
+
+    data class Question(var q: String, var answer: Boolean){
 
     }
 
-    data class question(var q: String, var answer: Boolean)
+    data class Quiz(val questions: List<Question>){
 
+    }
 
     fun wireWidgits() {
         questionText = findViewById(R.id.textView_main_question)
